eda4b5a25ab4e2d8c75aa390c875e2545f8ea585
Jason Chevrie <jason.chevrie@iit.it>
2018-11-16 14:51:23 +0100

    Add parameters for the configuration of the camera calibration (principal point, focal length)

--- modules/vision3d-gateway/src/main.cpp
+++ modules/vision3d-gateway/src/main.cpp
@@ -30,10 +30,20 @@ class Gateway : public RFModule
     RpcClient camPort;
 
     bool camera_configured;
-    double fov_h,fov_v;
+
+    double fx,fy;
+    bool camera_focalLength_configured;
+
+    double ppx,ppy;
+    bool camera_principalPoint_configured;
+
+    double fov_x,fov_y;
+    bool camera_fov_configured;
 
     Mutex depthMutex;
     ImageOf<PixelFloat> depth;
+    bool camera_resolution_configured;
+
     Matrix Hcam;
 
     /****************************************************************/
@@ -41,21 +51,99 @@ class Gateway : public RFModule
     {
         if (camPort.getOutputCount()>0)
         {
-            Bottle cmd,rep;
+            Bottle cmd,rep;   
             cmd.addVocab(Vocab::encode("visr"));
             cmd.addVocab(Vocab::encode("get"));
-            cmd.addVocab(Vocab::encode("fov"));
+            cmd.addVocab(Vocab::encode("intp"));
+            if (camPort.write(cmd,rep))
+            {
+                if (rep.size()>=4)
+                {
+                    const Bottle *list = rep.get(3).asList();
+
+                    ppx = list->find("principalPointX").asDouble();
+                    ppy = list->find("principalPointY").asDouble();
+                    camera_principalPoint_configured=true;
+                    yInfo()<<"camera principal point X (from sensor) ="<<ppx;
+                    yInfo()<<"camera principal point Y (from sensor) ="<<ppy;
+
+                    fx = list->find("focalLengthX").asDouble();
+                    fy = list->find("focalLengthY").asDouble();
+                    camera_focalLength_configured=true;
+                    yInfo()<<"camera focal length X (from sensor) ="<<fx;
+                    yInfo()<<"camera focal length Y (from sensor) ="<<fy;
+                }
+            }
+
+            cmd.clear();
+            cmd.addVocab(Vocab::encode("visr"));
+            cmd.addVocab(Vocab::encode("get"));
+            cmd.addVocab(Vocab::encode("res"));
             if (camPort.write(cmd,rep))
             {
                 if (rep.size()>=5)
                 {
-                    fov_h=rep.get(3).asDouble();
-                    fov_v=rep.get(4).asDouble();
-                    yInfo()<<"camera fov_h (from sensor) ="<<fov_h;
-                    yInfo()<<"camera fov_v (from sensor) ="<<fov_v;
-                    return true;
+                    depth.resize(rep.get(3).asDouble(), rep.get(4).asDouble());
+                    camera_resolution_configured=true;
+                    yInfo()<<"camera resolution X (from sensor) ="<<depth.width();
+                    yInfo()<<"camera resolution Y (from sensor) ="<<depth.height();
+
+                    if (!camera_principalPoint_configured)
+                    {
+                        ppx = 0.5*depth.width();
+                        ppy = 0.5*depth.height();
+                        camera_principalPoint_configured=true;
+                        yInfo()<<"camera principal point X (from sensor res) ="<<ppx;
+                        yInfo()<<"camera principal point Y (from sensor res) ="<<ppy;
+                    }
+                    if (camera_focalLength_configured)
+                    {
+                        fov_x = 2.0*(180.0/M_PI)*atan(0.5*depth.width()/fx);
+                        fov_y = 2.0*(180.0/M_PI)*atan(0.5*depth.height()/fy);
+                        camera_fov_configured=true;
+                        yInfo()<<"camera horizontal field of view (from sensor res+focalLength) ="<<fov_x;
+                        yInfo()<<"camera vertical field of view (from sensor res+focalLength) ="<<fov_y;
+                    }
                 }
             }
+
+            if (!camera_fov_configured)
+            {
+                cmd.clear();
+                cmd.addVocab(Vocab::encode("visr"));
+                cmd.addVocab(Vocab::encode("get"));
+                cmd.addVocab(Vocab::encode("fov"));
+                if (camPort.write(cmd,rep))
+                {
+                    if (rep.size()>=5)
+                    {
+                        fov_x=rep.get(3).asDouble();
+                        fov_y=rep.get(4).asDouble();
+                        camera_fov_configured=true;
+                        yInfo()<<"camera horizontal field of view (from sensor) ="<<fov_x;
+                        yInfo()<<"camera vertical field of view (from sensor) ="<<fov_y;
+
+                        if(camera_resolution_configured && !camera_focalLength_configured)
+                        {
+                            fx = depth.width()/(2.0*tan(fov_x*(M_PI/180.0)/2.0));
+                            fy = depth.height()/(2.0*tan(fov_y*(M_PI/180.0)/2.0));
+                            camera_focalLength_configured=true;
+                            yInfo()<<"camera focal length X (from sensor fov) ="<<fx;
+                            yInfo()<<"camera focal length Y (from sensor fov) ="<<fy;
+                        }
+
+                        if (!camera_resolution_configured && camera_focalLength_configured)
+                        {
+                            double width=fx*2.0*tan(fov_x*(M_PI/180.0)/2.0);
+                            double height=fy*2.0*tan(fov_y*(M_PI/180.0)/2.0);
+                            depth.resize(width,height);
+                            yInfo()<<"camera res X (from sensor fov+focalLength) ="<<depth.width();
+                            yInfo()<<"camera res Y (from sensor fov+focalLength) ="<<depth.height();
+                        }
+                    }
+                }
+            }
+            return camera_principalPoint_configured && camera_focalLength_configured && camera_resolution_configured;
         }
         return false;
     }
@@ -64,19 +152,17 @@ class Gateway : public RFModule
     Vector getPoint3D(const int u, const int v) const
     {
         Vector p(3,0.0);
-        if ((u>=0) && (u<depth.width()) && (v>=0) && (v<depth.height()) && (fov_h>0.0) && (fov_v>0.0))
+        if ((u>=0) && (u<depth.width()) && (v>=0) && (v<depth.height()))
         {
-            double f_h=depth.width()/(2.0*tan(fov_h*(M_PI/180.0)/2.0));
-            double f_v=depth.height()/(2.0*tan(fov_v*(M_PI/180.0)/2.0));
             double d=depth(u,v);
-            if ((d>0.0) && (f_h>0.0) && (f_v>0.0))
+            if ((d>0.0) && (fx>0.0) && (fy>0.0))
             {
-                double x=u-0.5*(depth.width()-1);
-                double y=v-0.5*(depth.height()-1);
+                double x=u-ppx;
+                double y=v-ppy;
 
-                p=d*ones(3);
-                p[0]*=x/f_h;
-                p[1]*=y/f_v;
+                p[0]=x*d/fx;
+                p[1]=y*d/fy;
+                p[2]=d;
             }
         }
         return p;
@@ -86,29 +172,131 @@ class Gateway : public RFModule
     bool configure(ResourceFinder &rf) override
     {
         // default values
+
         camera_configured=false;
+
+        ppx=0;
+        ppy=0;
+        camera_principalPoint_configured=false;
+
+        fx=0;
+        fy=0;
+        camera_focalLength_configured=false;
+
+        fov_x=0;
+        fov_y=0;
+        camera_fov_configured=false;
+
+        camera_resolution_configured=false;
+
         Hcam=eye(4,4);
-        fov_h = 0;
-        fov_v = 0;
 
         // retrieve values from config file
         Bottle &gCamera=rf.findGroup("camera");
         if (!gCamera.isNull())
         {
+            if (gCamera.check("principalPoint"))
+            {
+                if (Bottle *pp=gCamera.find("principalPoint").asList())
+                {
+                    if (pp->size()>=2)
+                    {
+                        ppx=pp->get(0).asDouble();
+                        ppy=pp->get(1).asDouble();
+                        camera_principalPoint_configured=true;
+                        yInfo()<<"camera principal point X (from file) ="<<ppx;
+                        yInfo()<<"camera principal point Y (from file) ="<<ppy;
+                    }
+                }
+            }
+
+            if (gCamera.check("focalLength"))
+            {
+                if (Bottle *fl=gCamera.find("focalLength").asList())
+                {
+                    if (fl->size()>=2)
+                    {
+                        fx=fl->get(0).asDouble();
+                        fy=fl->get(1).asDouble();
+                        camera_focalLength_configured=true;
+                        yInfo()<<"camera focal length X (from file) ="<<fx;
+                        yInfo()<<"camera focal length Y (from file) ="<<fy;
+                    }
+                }
+            }
+
+            if (gCamera.check("res"))
+            {
+                if (Bottle *res=gCamera.find("res").asList())
+                {
+                    if (res->size()>=2)
+                    {
+                        depth.resize(res->get(0).asDouble(), res->get(1).asDouble());
+                        camera_resolution_configured=true;
+                        yInfo()<<"camera resolution X (from file) ="<<depth.width();
+                        yInfo()<<"camera resolution Y (from file) ="<<depth.height();
+
+                        if (!camera_principalPoint_configured)
+                        {
+                            ppx = 0.5*depth.width();
+                            ppy = 0.5*depth.height();
+                            camera_principalPoint_configured=true;
+                            yInfo()<<"camera principal point X (from file res) ="<<ppx;
+                            yInfo()<<"camera principal point Y (from file res) ="<<ppy;
+                        }
+                        if (camera_focalLength_configured)
+                        {
+                            fov_x = 2.0*(180.0/M_PI)*atan(0.5*depth.width()/fx);
+                            fov_y = 2.0*(180.0/M_PI)*atan(0.5*depth.height()/fy);
+                            camera_fov_configured=true;
+                            yInfo()<<"camera horizontal field of view (from file res+focalLength) ="<<fov_x;
+                            yInfo()<<"camera vertical field of view (from file res+focalLength) ="<<fov_y;
+                        }
+                    }
+                }
+            }
+
             if (gCamera.check("fov"))
             {
                 if (Bottle *fov=gCamera.find("fov").asList())
                 {
                     if (fov->size()>=2)
                     {
-                        camera_configured=true;
-                        fov_h=fov->get(0).asDouble();
-                        fov_v=fov->get(1).asDouble();
-                        yInfo()<<"camera fov_h (from file) ="<<fov_h;
-                        yInfo()<<"camera fov_v (from file) ="<<fov_v;
+                        if (camera_fov_configured)
+                        {
+                            yError()<<"Configuration file contains incompatible redundant parameters (focalLength+resolution+fov)";
+                        }
+                        else
+                        {
+                            fov_x=fov->get(0).asDouble();
+                            fov_y=fov->get(1).asDouble();
+                            camera_fov_configured=true;
+                            yInfo()<<"camera horizontal field of view (from file) ="<<fov_x;
+                            yInfo()<<"camera vertical field of view (from file) ="<<fov_y;
+
+                            if(camera_resolution_configured && !camera_focalLength_configured)
+                            {
+                                fx = depth.width()/(2.0*tan(fov_x*(M_PI/180.0)/2.0));
+                                fy = depth.height()/(2.0*tan(fov_y*(M_PI/180.0)/2.0));
+                                camera_focalLength_configured=true;
+                                yInfo()<<"camera focal length X (from file fov) ="<<fx;
+                                yInfo()<<"camera focal length Y (from file fov) ="<<fy;
+                            }
+
+                            if (!camera_resolution_configured && camera_focalLength_configured)
+                            {
+                                double width=fx*2.0*tan(fov_x*(M_PI/180.0)/2.0);
+                                double height=fy*2.0*tan(fov_y*(M_PI/180.0)/2.0);
+                                depth.resize(width,height);
+                                  yInfo()<<"camera res X (from file fov+focalLength) ="<<depth.width();
+                                yInfo()<<"camera res Y (from file fov+focalLength) ="<<depth.height();
+                            }
+                        }
                     }
                 }
             }
+
+            camera_configured = camera_principalPoint_configured && camera_focalLength_configured && camera_resolution_configured;
         }
 
         depthPort.open("/vision3d-gateway/depth:i");
@@ -137,8 +325,13 @@ class Gateway : public RFModule
 
         if (ImageOf<PixelFloat> *ptr=depthPort.read(true))
         {
+            if (camera_resolution_configured && ((ptr->width()!=depth.width()) || (ptr->height()!=depth.height())) )
+            {
+                yInfo()<<"Warning received depth image dimensions do not match configuration";
+            }
             LockGuard lg(depthMutex);
             depth=*ptr;
+            camera_resolution_configured=true;
         }
         if (Property *ptr=gazePort.read(false))
         {
